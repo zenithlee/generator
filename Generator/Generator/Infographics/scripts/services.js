f79118dd23b217b27d3c12113df1781aefda1ff6
Karl Lilje <karll@zctkarlil002.mgsops.net>
2016-05-04 15:23:21 +0200

    banner and service

---
+++ Generator/Generator/Infographics/scripts/services.js
@@ -0,0 +1,13 @@
+﻿function GetDataFromService() {
+    var result = $.get(URL);
+
+    result.success(function(e){
+        console.log(e);
+    })
+}
+
+function GetData() {
+    $.getJSON("http://sentientlabs.co/infographics/service/a=123", function (e) {
+        console.log(e);
+    });
+}
