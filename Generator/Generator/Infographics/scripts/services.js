952bee796808e53e80bdbe3e2802557f4288f1e2
Karl Lilje <karll@zctkarlil002.mgsops.net>
2016-05-04 16:00:52 +0200

    graphing

--- Generator/Generator/Infographics/scripts/services.js
+++ Generator/Generator/Infographics/scripts/services.js
@@ -10,4 +10,73 @@ function GetData() {
     $.getJSON("http://sentientlabs.co/infographics/service/a=123", function (e) {
         console.log(e);
     });
+
+    CreateDemoGraph();
+}
+
+var config = {
+    type: 'pie',
+    data: {
+        datasets: [{
+            data: [
+                10,
+                20,
+                50,
+                12,
+                14,
+            ],
+            backgroundColor: [
+                "#F7464A",
+                "#46BFBD",
+                "#FDB45C",
+                "#949FB1",
+                "#4D5360",
+            ],
+        }, {
+            data: [
+                10,
+                20,
+                50,
+                12,
+                14,
+            ],
+            backgroundColor: [
+                "#F7464A",
+                "#46BFBD",
+                "#FDB45C",
+                "#949FB1",
+                "#4D5360",
+            ],
+        }, {
+            data: [
+                10,
+                20,
+                50,
+                12,
+                14,
+            ],
+            backgroundColor: [
+                "#F7464A",
+                "#46BFBD",
+                "#FDB45C",
+                "#949FB1",
+                "#4D5360",
+            ],
+        }],
+        labels: [
+            "Red",
+            "Green",
+            "Yellow",
+            "Grey",
+            "Dark Grey"
+        ]
+    },
+    options: {
+        responsive: true
+    }
+};
+
+function CreateDemoGraph() {
+    var ctx = document.getElementById("chart-area").getContext("2d");
+    window.myPie = new Chart(ctx, config);
 }
